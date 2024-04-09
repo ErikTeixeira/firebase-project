2561b803c2164e246d6a9b3213323aa7c532ddd4
ErikTeixeira <erik.barbosa515@gmail.com>
2024-04-09 09:47:52 -0300

    projeto firebase

--- App.js
+++ App.js
@@ -1,11 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { app } from './firebaseConfig';
 
 export default function App() {
+  const [email, setEmail] = React.useState('');
+  const [pass, setPass] = React.useState('');
+
+  const [error, setErrror] = React.useState('');
+
+  const createUser = () => {
+    const auth = getAuth(app);
+
+    createUserWithEmailAndPassword(auth, email, pass)
+      .then(userCredentials => {
+        console.log("Sucessooo!!",
+        userCredentials.user);
+      })
+      .catch((error) => {
+        console.log("Error!!", error)
+      })
+      .finally(() => {
+        console.log("Finall!!")
+      })
+  }
+
+  const loginUser = () => {
+    const auth = getAuth(app);
+
+    signInWithEmailAndPassword(auth, email, pass)
+      .then(userCredentials => {
+        console.log("Sucessooo!!",
+        userCredentials.user);
+      })
+      .catch((error) => {
+        console.log("Error!!", error)
+      })
+      .finally(() => {
+        console.log("Finall!!")
+      })
+  }
+
+  const logout = () => {
+    const auth = getAuth(app);
+    auth.signOut()
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{"Voce nao esta logado"}</Text>
+      <TextInput 
+        style={styles.input} 
+        value={email}
+        onChangeText={setEmail}
+        placeholder='email'
+      />
+      <TextInput 
+        style={styles.input} 
+        value={pass}
+        onChangeText={setPass}
+        placeholder='senha'
+      />
+
+      <Button title='Login' onPress={loginUser} />
+      <Button title='Cadastrar' onPress={createUser} />
     </View>
   );
 }
@@ -15,6 +73,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
 });
---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1QvFaH09iTSVeI8SQDuPe8pzLxVM9FKc",
+  authDomain: "aula-mobile-fiap-2024-4e888.firebaseapp.com",
+  projectId: "aula-mobile-fiap-2024-4e888",
+  storageBucket: "aula-mobile-fiap-2024-4e888.appspot.com",
+  messagingSenderId: "447021829996",
+  appId: "1:447021829996:web:418928907bd995d7d9ad49"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
